672fdd165beb5da3387b91944a51b612ddf51b33
Pavithran Thilakanathan <pavithranthilakathan@gmail.com>
2023-07-17 10:29:29 +1000

    move scripts to scripts folder

--- scripts/clean_script.py
+++ scripts/clean_script.py
@@ -1,8 +1,13 @@
+# This is a script for extracting and saving the action unit intensity columns from the feature maps
+
 import os
 from tqdm import tqdm
 import pandas as pd
 
+# Features input path
 feature_path = 'Features/fake/features'
+
+# Feature map output path
 out_path = 'Feature_Maps/Fake/'
 feature_count = 0
 
@@ -12,8 +17,12 @@ for filename in tqdm(os.listdir(feature_path)):
     feature_count += 1
     
     df = pd.read_csv(f)
+    
+    # Get the action unit intensity columns from the dataframe
     df = df.loc[:, ' AU01_r':' AU45_r']
+    # Strip whitespace
     df.columns = df.columns.str.replace(' ', '')
+    # Set label fake or real
     df['label'] = 'fake'
     
     save_path = out_path + str(feature_count) + '_fake.csv'
--- scripts/video_trimmer.py
+++ scripts/video_trimmer.py
@@ -1,18 +1,27 @@
+# This is a script for trimming video files to 10secs
+
 from moviepy.editor import *
 import os
 from tqdm import tqdm
 
+# Video input path
 real_path = 'Celeb-synthesis'
+
+# Features output path
 feature_path_r = 'Features/fake/'
 feature_map_count = 0
 
 print("Beginning trimming----------------------")
+# Iterate over the files in the directory
 for filename in tqdm(os.listdir(real_path)):
     f = os.path.join(real_path, filename)
     feature_map_count += 1
     map_name = '.mp4'
     
+    # Use the VideoFileClip class from moviepy library
     clip = VideoFileClip(f)
+    
+    # Trim the the video to 10 secs
     subclip1 = clip.subclip((0,00), (0,10))
     subclip1.write_videofile(feature_path_r+str(feature_map_count)+map_name)
     
